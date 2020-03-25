48e1ebc567c680862cc499122b73300fcb04af85
terryky <terryky1220@gmail.com>
2020-03-25 22:53:36 +0900

    update log function

--- common/util_debug.h
+++ common/util_debug.h
@@ -9,10 +9,8 @@
 #include <android/log.h>
 #define LOG_TAG "gles_app"
 #define DBG_LOGI(...) __android_log_print(ANDROID_LOG_INFO,  LOG_TAG, __VA_ARGS__)
+#define DBG_LOGW(...) __android_log_print(ANDROID_LOG_WARN,  LOG_TAG, __VA_ARGS__)
 #define DBG_LOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
-
-
-
 
 #define DBG_ASSERT(cond, ...) do {                                  \
     if (!(cond)) {                                                  \
@@ -22,6 +20,20 @@
     } \
 } while(0)
 
+#ifndef LOGI
+#define LOGI DBG_LOGI
+#endif
 
+#ifndef LOGW
+#define LOGW DBG_LOGW
+#endif
+
+#ifndef LOGE
+#define LOGE DBG_LOGE
+#endif
+
+#ifndef ASSERT
+#define ASSERT DBG_ASSERT
+#endif
 
 #endif /* _UTIL_DEBUG_H_ */
