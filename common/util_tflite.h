ae6ce30b6c9ccc671b5c0564c3b175e0011b57b0
terryky <terryky1220@gmail.com>
2020-03-27 09:28:49 +0900

    update util_tflite

--- common/util_tflite.h
+++ common/util_tflite.h
@@ -28,6 +28,21 @@ typedef struct tflite_interpreter_t
 } tflite_interpreter_t;
 
 
+typedef struct tflite_tensor_t
+{
+    int         idx;        /* whole  tensor index */
+    int         io;         /* [0] input_tensor, [1] output_tensor */
+    int         io_idx;     /* in/out tensor index */
+    TfLiteType  type;       /* [1] kTfLiteFloat32, [2] kTfLiteInt32, [3] kTfLiteUInt8 */
+    void        *ptr;
+    int         dims[4];
+    float       quant_scale;
+    int         quant_zerop;
+} tflite_tensor_t;
+
+
+
 int tflite_create_interpreter (tflite_interpreter_t *p, const char *model_buf, size_t model_size);
+int tflite_get_tensor_by_name (tflite_interpreter_t *p, int io, const char *name, tflite_tensor_t *ptensor);
 
 #endif /* _UTIL_TFLITE_H_ */
