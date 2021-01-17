c987c63309e388390ba6b68927e4271e3cd7d540
terryky <terryky1220@gmail.com>
2021-01-17 19:02:17 +0900

    added script file to download assets

---
+++ download_all_assets.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+
+
+echo "============= download assets for tflite_face_portrait ==============="
+cd ${SCRIPT_DIR}/tflite_face_portrait/app/src/main/assets/model/
+./download.sh
+
+echo "============= download assets for tflite_dense_depth ==============="
+cd ${SCRIPT_DIR}/tflite_dense_depth/app/src/main/assets/model/
+./download.sh
+
