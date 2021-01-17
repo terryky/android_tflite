8f51a89200cab3c28b33fe989ce9e211c464d4b6
terryky <terryky1220@gmail.com>
2021-01-17 19:00:09 +0900

    changed url

--- tflite_dense_depth/app/src/main/assets/model/download.sh
+++ tflite_dense_depth/app/src/main/assets/model/download.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=1WpsN4D9N2JXAlbbimxK9KbzY_Fzf00tN" > /dev/null
+curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=1bW9Ip5kXNFcdvHC0dP2p7hSPw2JU1lyE" > /dev/null
 CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
-curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=1WpsN4D9N2JXAlbbimxK9KbzY_Fzf00tN" -o resources.tar.gz
+curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=1bW9Ip5kXNFcdvHC0dP2p7hSPw2JU1lyE" -o resources.tar.gz
 tar -zxvf resources.tar.gz
 rm resources.tar.gz
 
